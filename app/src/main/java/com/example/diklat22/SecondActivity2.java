c52c7bae82c876c0b9ac7b211fcb9369f964441e
H1sMaj3sty <akuncadangannn2020@gmail.com>
2023-10-31 10:17:44 +0700

    Commit 2, penambahan data pada SecondActivity2

--- app/src/main/java/com/example/diklat22/SecondActivity2.java
+++ app/src/main/java/com/example/diklat22/SecondActivity2.java
@@ -1,16 +1,34 @@
 package com.example.diklat22;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 
-public class SecondActivity2 extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class SecondActivity2 extends AppCompatActivity {
+    private ArrayList<itemData> itemValues;
+    private RecyclerView recyclerView;
+    private itemAdapter itemAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second2);
+        recyclerView=findViewById(R.id.recycler_view);
+        itemValues=new ArrayList<>();
+        for(int a = 1; a < 11; a++) {
+            itemData item = new itemData();
+            item.itemTitle="Title Data " + a;
+            item.itemSubTitle="Subtitle Data " + a;
+            itemValues.add(item);
+        }
+        itemAdapter = new itemAdapter(this, itemValues);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(itemAdapter);
     }
 
     public void addData(View view) {
